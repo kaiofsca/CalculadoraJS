2ae58232f61024ac723fdaf11965d20e15e27825
Kaio Fonseca <mkaiofonseca@gmail.com>
2022-08-17 15:11:37 -0300

    refazendo e terminando projetinho

--- js/calculador.js
+++ js/calculador.js
@@ -1,13 +1,23 @@
-'use strict' // instrui o navegador para usar o modo Estrito, que é como se fosse uma versão mais segura e reduzida do JavaScript.
 
-const display = document.getElementById('display')
-const numeros = document.querySelectorAll('[id*=tecla]') // "*" qualquer elemento que tenha "tecla no nome"
-const operadores = document.querySelectorAll('[id*=operador]')
-
-const atualizarDisplay = (texto) => {
-    display.textContent += texto
+function insert(num) {
+    let numero = document.getElementById('resultado').innerHTML
+    document.getElementById('resultado').innerHTML = numero + num;
 }
 
-const inserirNumero = (evento) => atualizarDisplay(evento.target.textContent) // recebe do addEventListener um evento
+function clean() {
+    document.getElementById('resultado').innerHTML = ''
+}
 
-numeros.forEach (numero => numero.addEventListener('click', inserirNumero)) // add evento em todos os numeros.
+function back() {
+    let resultado = document.getElementById('resultado').innerHTML
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1)
+}
+
+function calcular() {
+    let resultado = document.getElementById('resultado').innerHTML
+    if(resultado) {
+        document.getElementById('resultado').innerHTML = eval(resultado) // eval uma funçao que ja vai computar todo nosso resultado 
+    } else {
+        alert('Nada para calcular!')
+    }
+}
